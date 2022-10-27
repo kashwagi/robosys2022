526e79bf6fdf2e37e6cd7e4384a1cbe8228e5dfb
Yuki Kashiwagi <ponpon@ponmail.com>
2022-10-27 12:25:25 +0900

    add license and easier to check

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yuki Kashiwagi
+# SPDX-License-Identifier: BSD-3-Clause
+　
+ng () {
+	echo NG at Line $1
+	res=1
+}
 
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
