987acfbb3883925911fc68862651e48dfe23b11a
Yuki Kashiwagi <ponpon@ponmail.com>
2022-11-30 01:13:26 +0900

    テスト項目の追加

--- test.bash
+++ test.bash
@@ -16,3 +16,15 @@ out=$(seq 5 | ./plus)
 
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
+
+### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    
+[ "$res" = 0 ] && echo OK
+  exit $res
