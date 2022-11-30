3536e52bd0b037387b47d32eb760c108b837d9eb
Yuki Kashiwagi <ponpon@ponmail.com>
2022-12-01 01:27:01 +0900

    修正

--- test.bash
+++ test.bash
@@ -14,21 +14,24 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-exit $res
+
 
 ### STRANGE INPUT ###
-  out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
     
-  out=$(echo | ./plus) #空文字
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-    
-[ "$res" = 0 ] && echo OK
-  exit $res
 
 #count test
-out=$(./count pppppppppp)
+out=$(./count.py pppppppppp)
 [ "${out}" = 10 ] || ng ${LINENO}
+out=$(./count.py) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
