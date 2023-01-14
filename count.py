d9a126d14e0f9873758760c9b827aa0b40eead6d
Yuki Kashiwagi <ponpon@ponmail.com>
2023-01-14 14:29:23 +0900

    SPDX識別子修正

--- count.py
+++ count.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# SPDX-FileCopyrightText: 2022 Yuki Kashiwagi
+
+# SPDX-FileCopyrightText: 2023  Yuki Kashiwagi
+# SPDX-License-Identifier: BSD-3-Clause
 import sys
 mozi = sys.argv[1]
 print(len(mozi))
